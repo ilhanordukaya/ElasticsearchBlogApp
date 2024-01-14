9c8e4eabd6c5e0a462153aac88cbdaa4e38425b9
ilhanordukaya <ilhanordukaya69@gmail.com>
2024-01-14 05:17:14 +0300

    added ecommerrce module

---
+++ Elasticsearch.Web/Elasticsearch.Web/Services/ECommerceService.cs
@@ -0,0 +1,56 @@
+﻿using Elasticsearch.Web.Repositories;
+using Elasticsearch.Web.ViewModels;
+
+namespace Elasticsearch.Web.Services
+{
+	public class ECommerceService
+	{
+		private readonly ECommerceRepository _eCommerceRepository;
+
+		public ECommerceService(ECommerceRepository eCommerceRepository)
+		{
+			_eCommerceRepository = eCommerceRepository;
+		}
+
+		public async Task<(List<ECommerceViewModel>, long totalCount, long pageLinkCount)> SearchAsync(ECommerceSearchViewModel searchModel, int page, int pageSize)
+		{
+			var (eCommerceList, totalCount) = await _eCommerceRepository.SearchAsync(searchModel, page, pageSize);
+
+			var pageLinkCountCalculate = totalCount % pageSize;
+
+			long pageLinkCount = 0;
+
+			if (pageLinkCountCalculate == 0)
+			{
+				pageLinkCount = totalCount / pageSize;
+			}
+			else
+			{
+				pageLinkCount = (totalCount / pageSize) + 1;
+
+			}
+
+			var eCommerceListViewModel = eCommerceList.Select(x => new ECommerceViewModel()
+			{
+				Category = String.Join(",", x.Category),
+				CustomerFullName = x.CustomerFullName,
+				CustomerFirstName = x.CustomerFirstName,
+				CustomerLastName = x.CustomerLastName,
+				OrderDate = x.OrderDate.ToShortDateString(),
+				Gender = x.Gender.ToLower(),
+				Id = x.Id,
+				OrderId = x.OrderId,
+				TaxfulTotalPrice = x.TaxfulTotalPrice
+
+
+
+			}).ToList();
+
+
+			return (eCommerceListViewModel, totalCount, pageLinkCount);
+
+
+
+		}
+	}
+}
